4db25bfe655496650619b2d0997719a0d14938be
Rahul <rahul98112005@gmail.com>
2025-08-03 13:54:00 +0530

    fix typo error

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion,syncUserUpdation } from "@/config/inngest";
+import User from "@/models/user"
 
 export const  { GET, POST, PUT} = serve ({
     client: inngest,
